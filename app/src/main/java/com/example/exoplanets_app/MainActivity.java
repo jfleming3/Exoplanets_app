39e0c23d79062d92a5792cbc73e080373cdd8bfc
justin <flem3@outlookc.om>
2021-03-08 19:34:38 -0600

    Database 1.3

--- app/src/main/java/com/example/exoplanets_app/MainActivity.java
+++ app/src/main/java/com/example/exoplanets_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exoplanets_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,13 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+            public void onDataChange(DataSnapshot dataSnapshot){
 
-              //  Planet gj = new Planet("GJ 1061 c","Gliese 1061","12","6.7","221,387");
+
+               // System.out.println(myRef.child("GJ 1061 c").child("Travel").get();
+
+               //Planet gj = new Planet("GJ 1061 c","Gliese 1061","12","6.7","221,387");
                //myRef.setValue(gj);
                 String value = dataSnapshot.getValue(String.class);
                 Log.d("CIS3334", "Value is: " + value);
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-               Log.w("CIS3334", "Failed to read value.", error.toException());
+              // Log.w("CIS3334", "Failed to read value.", error.toException());
             }
         });
 
